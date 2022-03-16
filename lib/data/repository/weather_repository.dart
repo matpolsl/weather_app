246e2d000675572a1578e740fed20ef73dd219dc
matpolsl <56276428+matpolsl@users.noreply.github.com>
2022-03-16 21:01:11 +0100

    fixed error handling

--- lib/data/repository/weather_repository.dart
+++ lib/data/repository/weather_repository.dart
@@ -11,25 +11,21 @@ class WeatherRepository {
   final http.Client httpClient;
   WeatherRepository(this.httpClient);
   Future<Weather> fetchWeatherFromCity(String city) async {
-    try {
-      final response = await httpClient.get(Uri.parse(weatherUrl(city)));
-      switch (response.statusCode) {
-        case 200:
-          return Weather.fromJson(response.body);
-        case 400:
-          throw FetchDataException("Nie wprowadzono miasta");
-        case 401:
-        case 403:
-          throw FetchDataException(response.body.toString());
-        case 404:
-          throw FetchDataException("Nie znaleziono pogody dla miasta: $city");
-        case 500:
-        default:
-          throw FetchDataException(
-              'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
-      }
-    } catch (e) {
-      throw FetchDataException("Błąd połączenia z internetem");
+    final response = await httpClient.get(Uri.parse(weatherUrl(city)));
+    switch (response.statusCode) {
+      case 200:
+        return Weather.fromJson(response.body);
+      case 400:
+        throw FetchDataException("Nie wprowadzono miasta");
+      case 401:
+      case 403:
+        throw FetchDataException(response.body.toString());
+      case 404:
+        throw FetchDataException("Nie znaleziono pogody dla miasta: $city");
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
     }
   }
 }
